ceb79a5b86b21ed35306c126747615b299c3da22
jguimeradev <debian@PCJGM.localdomain>
2025-10-02 16:41:23 +0200

    first typescript file

---
+++ src/scripts/selector.ts
@@ -0,0 +1,17 @@
+type Menu = { name: string, id: number }
+
+export const MenuList: Array<Menu> = [
+    {
+        name: "blog",
+        id: 1
+    },
+    {
+        name: "snake",
+        id: 2
+    },
+    {
+        name: "About",
+        id: 3
+    }
+
+]
