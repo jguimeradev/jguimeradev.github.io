03f0b579b95b487680c98feebde98875d4129fad
jguimeradev <jguimeradev@gmail.com>
2025-10-04 07:04:14 +0200

    load selected content

--- src/scripts/script.ts
+++ src/scripts/script.ts
@@ -1,25 +1,42 @@
-const options: NodeListOf<HTMLLIElement> = document.querySelectorAll(".menu-option li");
-
-const cursor: HTMLSpanElement = document.createElement("span");
-cursor.classList.add("cursor");
-
-let currentIndex: number = 0;
-
-// set cursor on the first item
-options[currentIndex].insertBefore(cursor, options[currentIndex].firstChild);
-
-document.addEventListener("keydown", (e: KeyboardEvent): void => {
-  if (e.key === "ArrowDown") {
-    currentIndex++;
-  } else if (e.key === "ArrowUp") {
-    currentIndex--;
-  } else if (e.key === "Enter") {
-    const id: string | null = options[currentIndex].getAttribute("data-id");
-    console.log(id);
-  }
-
-  if (currentIndex === options.length) currentIndex = 0;
-  if (currentIndex < 0) currentIndex = options.length - 1;
-
-  options[currentIndex].insertBefore(cursor, options[currentIndex].firstChild);
+document.addEventListener('DOMContentLoaded', () => {
+  TerminalMenu();
 });
+
+function TerminalMenu() {
+    
+    const options: NodeListOf<HTMLLIElement> = document.querySelectorAll(".menu-option li");
+    const content: NodeListOf<HTMLDivElement> = document.querySelectorAll('.content')
+    const cursor: HTMLSpanElement = document.createElement("span");
+    cursor.classList.add("cursor");
+    
+    let currentIndex: number = 0;
+    
+    // set cursor on the first item
+    options[currentIndex].insertBefore(cursor, options[currentIndex].firstChild);
+    
+    document.addEventListener("keydown", (e: KeyboardEvent): void => {
+        if (e.key === "ArrowDown") {
+            currentIndex++;
+        } else if (e.key === "ArrowUp") {
+            currentIndex--;
+        } else if (e.key === "Enter") {
+            const id: string | null = options[currentIndex].getAttribute("data-id");
+            for (const item of content) {
+                const itemId = item.getAttribute('data-id')
+                if (itemId === id) {
+                    item.classList.remove('content')
+                    item.classList.add('display')
+                } else {
+                    item.classList.remove('display')
+                    item.classList.add('content')
+                }
+            }
+        }
+        
+        if (currentIndex === options.length) currentIndex = 0;
+        if (currentIndex < 0) currentIndex = options.length - 1;
+        
+        options[currentIndex].insertBefore(cursor, options[currentIndex].firstChild);
+    });
+    
+}
